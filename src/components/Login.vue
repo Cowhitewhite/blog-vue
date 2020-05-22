0e18e002674bbcfff2532d1e4481b37a5efa275e
Cowhitewhite <960095492@qq.com>
2020-05-22 11:55:01 +0800

    init

---
+++ src/components/Login.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="materialContainer">
+    <div class="box">
+      <div class="title">登录</div>
+      <div class="input">
+        <label for="name">用户名</label>
+        <input type="text" name="name" id="name">
+        <span class="spin"></span>
+      </div>
+      <div class="input">
+        <label for="pass">密码</label>
+        <input type="password" name="pass" id="pass">
+        <span class="spin"></span>
+      </div>
+      <div class="button login">
+        <button>
+          <span>登录</span>
+          <i class="fa fa-check"></i>
+        </button>
+      </div>
+      <a href="javascript:" class="pass-forgot">忘记密码？</a>
+    </div>
+
+    <div class="overbox">
+      <div class="material-button alt-2">
+        <span class="shape"></span>
+      </div>
+      <div class="title">注册</div>
+      <div class="input">
+        <label for="regname">用户名</label>
+        <input type="text" name="regname" id="regname">
+        <span class="spin"></span>
+      </div>
+      <div class="input">
+        <label for="regpass">密码</label>
+        <input type="password" name="regpass" id="regpass">
+        <span class="spin"></span>
+      </div>
+      <div class="input">
+        <label for="reregpass">确认密码</label>
+        <input type="password" name="reregpass" id="reregpass">
+        <span class="spin"></span>
+      </div>
+      <div class="button">
+        <button>
+          <span>注册</span>
+        </button>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+</style>

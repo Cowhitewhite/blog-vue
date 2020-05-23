0975080f5d8d63dc77ad259ac1eebfbb319c3038
chenql <1078543989@qq.com>
2020-05-23 16:41:45 +0800

    NEW:背景图

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from '@/components/Login'
+import Login from '../components/Login'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
   routes: [
     {
       path: '/',
